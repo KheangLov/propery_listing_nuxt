b812e748317a2877cdcfc2f0b139027a32d8662d
kheanglov <lovsokheang@gmail.com>
2022-01-09 09:35:07 +0700

    remove script

--- layouts/default.vue
+++ layouts/default.vue
@@ -260,17 +260,17 @@ export default {
   },
   mounted() {
     this.handleActive();
-    document.addEventListener('contextmenu', e => e.preventDefault());
-    document.onkeydown = e => {
-      if(e.keyCode == 123
-        || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
-        || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
-        || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
-        || (e.metaKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))
-      ) {
-        return false;
-      }
-    }
+    // document.addEventListener('contextmenu', e => e.preventDefault());
+    // document.onkeydown = e => {
+    //   if(e.keyCode == 123
+    //     || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+    //     || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+    //     || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+    //     || (e.metaKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))
+    //   ) {
+    //     return false;
+    //   }
+    // }
   }
 }
 </script>
